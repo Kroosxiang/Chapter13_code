3a086286d127e095c37f238a8f151aed0b51d337
Kroos Xiang <kroosvi@gmail.com>
2023-02-12 19:45:15 -0500

    moving old main into main1 and starting new code practice with main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,10 @@
-#[derive(Debug,PartialEq,Copy,Clone)]
-enum ShirtColor {
-    Red,
-    Blue,
-}
+fn main(){
+    let list = vec![1,2,3];
+    println!("Before defining closure{:?} ", list);
 
-struct Inventory{
-    shirts: Vec<ShirtColor>,
-}
+    let only_borrows = || println!("From closure: {:?}",list);
 
-impl Inventory {
-    fn giveaway(&self, user_preference : Option<ShirtColor>) -> ShirtColor {
-        user_preference.unwrap_or_else(|| self.most_stock())// what is the ||? 
-    }
-
-    fn most_stock(&self) -> ShirtColor {
-        let mut num_red = 0;
-        let mut num_blue= 0;
-
-        for color in &self.shirts{
-            match color{
-                ShirtColor::Red => num_red +=1,
-                ShirtColor::Blue=> num_blue+=1,
-            }
-        }
-
-        if num_red > num_blue{
-            ShirtColor::Red
-        }
-        else{
-            ShirtColor::Blue
-        }
-    }
-}
-
-fn main() {
-    let store = Inventory{
-        // to illstrate the point we will use two blue and oen red
-        shirts : vec![ShirtColor::Blue,ShirtColor::Red , ShirtColor::Blue]
-    };
-
-    let user_pref1 = Some(ShirtColor::Red);
-    let giveaway1  = store.giveaway(user_pref1);
-
-    println!(
-        "The user with preference {:?} gets {:?}", 
-        user_pref1,giveaway1
-    );
-
-    let user_pref2= None;
-    let giveaway2 =store.giveaway(user_pref2);
-    println!(
-        "The user with preference {:?} gets {:?}"
-        , user_pref2,giveaway2
-    );
+    println!("Before calling closure : {:?}",list);
+    only_borrows();
+    println!("After calling closure : {:?}",list);
 }
